34ad80fea7e6b24fe2ef3617499abac36512df87
Mitch <michel@easy-target.eu>
2021-01-31 22:57:11 +0100

    chore: move files into appropriate dir after merge

---
+++ VPTree/Classes/VPNode.swift
@@ -0,0 +1,71 @@
+import Foundation
+
+private func <<T>(left: VPoint<T>, right: VPoint<T>) -> Bool {
+    return left.d < right.d
+}
+
+private func ==<T>(left: VPoint<T>, right: VPoint<T>) -> Bool {
+    return left.d == right.d
+}
+
+private final class VPoint<T>: Comparable {
+    var d: Double
+    let point: T
+    
+    init(d: Double, point: T) {
+        self.d = d
+        self.point = point
+    }
+}
+
+internal final class VPNode<T: Distance> {
+    var vpPoint: T!
+    var points: [T] = []
+    var mu: Double?
+    var leftChild: VPNode<T>?
+    var rightChild: VPNode<T>?
+    
+    convenience init?(elements: ArraySlice<T>) {
+        self.init(elements: [T](elements))
+    }
+    
+    private func convertion (item: VPoint<T>) -> T {
+        return item.point
+    }
+    
+    convenience init?(elements: [T]) {
+        let array: [VPoint<T>] = elements.map {
+            (item: T) -> VPoint<T> in
+            return VPoint<T>(d: 0, point: item)
+        }
+        
+        self.init(elements: array)
+    }
+    
+    private init?(elements: [VPoint<T>]) {
+        if elements.count == 0 {
+            return nil
+        }
+        var elements = elements
+        //Random get of the VP points
+        self.vpPoint = elements.remove(at: 0).point
+        
+        if elements.count == 0 {
+            return
+        }
+        
+        for item in elements {
+            item.d = item.point ~~ self.vpPoint
+        }
+        
+        let splitElements: (left: [VPoint<T>], right: [VPoint<T>]) = elements.splitByMedian()
+        
+        mu = splitElements.left.last!.d
+        leftChild = VPNode(elements: splitElements.left)
+        rightChild = VPNode(elements: splitElements.right)
+    }
+    
+    var isLeaf: Bool {
+        return leftChild == nil && rightChild == nil
+    }
+}
---
+++ VPTree/Classes/VPTree.swift
@@ -0,0 +1,64 @@
+infix operator ~~
+
+protocol Distance {
+    static func ~~(lhs: Self, rhs: Self) -> Double
+}
+
+final class VPTree<T: Distance> {
+    internal var firstNode: VPNode<T>
+    
+    init(elements: [T]) {
+        firstNode = VPNode<T>(elements: elements)!;
+    }
+    
+    private func _neighbors(point: T, limit: Int?, maxDistance: Double? = nil)
+    -> [T] {
+        var tau: Double = maxDistance ?? Double.infinity
+        var nodesToTest: [VPNode<T>?] = [firstNode]
+        
+        var neighbors = PriorityQueue<T>(limit: limit)
+        
+        while(nodesToTest.count > 0) {
+            if let node = nodesToTest.remove(at: 0) {
+                let d = point ~~ node.vpPoint
+                if d <= tau {
+                    neighbors.push(weight: d, item: node.vpPoint)
+                    if maxDistance == nil {
+                        tau = neighbors.biggestWeigth
+                    }
+                }
+                
+                if node.isLeaf {
+                    continue
+                }
+                
+                if d < node.mu! {
+                    if d < node.mu! + tau {
+                        nodesToTest.append(node.leftChild)
+                    }
+                    if d >= node.mu! - tau {
+                        nodesToTest.append(node.rightChild)
+                    }
+                } else {
+                    if d >= node.mu! - tau  {
+                        nodesToTest.append(node.rightChild)
+                    }
+                    if d < node.mu! + tau {
+                        nodesToTest.append(node.leftChild)
+                    }
+                }
+            }
+        }
+        
+        return neighbors.items
+    }
+    
+    func findNeighbors(point: T, limit: Int) -> [T] {
+        return _neighbors(point: point, limit: limit, maxDistance: nil)
+    }
+    
+    func findClosest(point: T, maxDistance: Double) -> [T] {
+        return _neighbors(point: point, limit: nil, maxDistance: maxDistance)
+    }
+    
+}
